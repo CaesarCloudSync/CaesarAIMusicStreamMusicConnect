44bf0243b013111a3a62f79aba79b5709be6ecc8
amari <amari.lawal@gmail.com>
2024-08-19 16:02:59 +0100

    adding window build to deployment

--- build_app.sh
+++ build_app.sh
@@ -1,2 +1,3 @@
-npm run make
-npm run make -- --arch=arm64 --targets=@electron-forge/maker-deb
+npm run make # Linux Build
+npm run make -- --arch=arm64 --targets=@electron-forge/maker-deb # Raspberry Pi Build
+npm run make -- --arch=x64 --targets=@electron-forge/maker-squirrel # Windows Build
