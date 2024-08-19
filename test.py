954e9dbb08bb66e40334efb5cac25b1e49c2ea3a
amari <amari.lawal@gmail.com>
2024-08-19 13:21:50 +0100

    using safe and correct wss for mqtt

--- test.py
+++
@@ -1,7 +0,0 @@
-import socketio
-const socket = io.connect("http://localhost:4000");
-# standard Python
-sio = socketio.SimpleClient()
-sio.connect('http://192.168.0.19:9090')
-sio.emit('welcome', {'foo': 'bar'})
-print('my sid is', sio.sid)
